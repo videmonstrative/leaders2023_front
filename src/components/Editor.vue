89a7c6998831c319b8cdf48dee8d45d4cef70760
Someone <someone@email.com>
2023-05-28 20:51:52 +0200

    Next commit

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -1,93 +1,38 @@
 <script setup>
-import Two from 'https://cdn.skypack.dev/two.js@latest'
-import {onMounted} from "vue";
 
+import {reactive} from "vue";
 
+const data = reactive([
+  {title: 'Первый расчет', result: [{item: 'A-1', material: '101', score: 0.8}, {item: '1-2', material: '500', score: 0.8}]},
+  {title: 'Первый расчет', result: [{item: 'A-1', material: '101', score: 0.8}, {item: '1-2', material: '500', score: 0.8}, {item: '2-3', material: '101', score: 0.8}]},
+])
 
-onMounted(() => {
-  // Создание холста Two.js
-  var elem = document.getElementById('drawArea');
-  var params = { width: 800, height: 600 };
-  var two = new Two(params).appendTo(elem);
-
-// Размеры сетки
-  var gridSize = 10;
-  var gridColor = 'red';
-
-// Переменные для хранения начальной и конечной точек линии
-  var startPoint, endPoint;
-  var line;
-
-// Создание привязочной сетки
-  var grid = two.makeGroup();
-  grid.stroke = 'blue';
-  grid.linewidth = 1;
-
-  for (var x = gridSize; x < params.width; x += gridSize) {
-    var verticalLine = two.makeLine(x, 0, x, params.height);
-    grid.add(verticalLine);
-  }
-
-  for (var y = gridSize; y < params.height; y += gridSize) {
-    var horizontalLine = two.makeLine(0, y, params.width, y);
-    grid.add(horizontalLine);
-  }
-
-// Обработчик события нажатия кнопки мыши
-  function onMouseDown(event) {
-    // Получение координат клика
-    var x = Math.floor((event.clientX - elem.getBoundingClientRect().left) / gridSize) * gridSize;
-    var y = Math.floor((event.clientY - elem.getBoundingClientRect().top) / gridSize) * gridSize;
-
-    // Создание начальной точки линии
-    startPoint = new Two.Vector(x, y);
-
-    // Создание новой линии
-    line = two.makeLine(startPoint.x, startPoint.y, startPoint.x, startPoint.y);
-    line.stroke = 'black';
-    line.linewidth = 5;
-  }
-
-// Обработчик события перемещения мыши
-  function onMouseMove(event) {
-    // Проверка, существует ли начальная точка линии
-    if (startPoint) {
-      // Получение текущих координат мыши
-      var x = Math.floor((event.clientX - elem.getBoundingClientRect().left) / gridSize) * gridSize;
-      var y = Math.floor((event.clientY - elem.getBoundingClientRect().top) / gridSize) * gridSize;
-
-      // Обновление конечной точки линии
-      endPoint = new Two.Vector(x, y);
-
-      // Обновление позиции и размера линии
-      line.vertices[1].set(endPoint.x, endPoint.y);
-    }
-  }
-
-// Обработчик события отпускания кнопки мыши
-  function onMouseUp(event) {
-    // Очистка переменных
-    startPoint = null;
-    endPoint = null;
-  }
-
-// Привязка обработчиков событий к холсту
-  elem.addEventListener('mousedown', onMouseDown);
-  elem.addEventListener('mousemove', onMouseMove);
-  elem.addEventListener('mouseup', onMouseUp);
-
-// Запуск анимации Two.js
-  function animate() {
-    two.update();
-    requestAnimationFrame(animate);
-  }
-  animate();
-
-})
 </script>
 
 <template>
-  <div id="drawArea" style="border: 1px solid gray; width: 800px; height: 600px"></div>
+  <div style="height: 20px"></div>
+  <div v-for="res in data">
+    <h6>{{ res.title }}</h6>
+    <div style="padding-bottom: 10px">
+      <input type="submit" class="btn btn-success btn-sm" value="Выгрузить в Excel">
+    </div>
+    <table class="table table-bordered" style="width: 50%">
+      <thead>
+      <tr>
+        <th scope="col">Граф</th>
+        <th scope="col">Материал</th>
+        <th scope="col">Вероятность</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for="row in res.result">
+        <td>{{ row.item }}</td>
+        <td>{{ row.material }}</td>
+        <td>{{ row.score }}</td>
+      </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <style scoped>
--- src/components/LeftSide.vue
+++ src/components/LeftSide.vue
@@ -1,29 +1,26 @@
 <template>
   <div class="d-flex flex-column align-items-stretch flex-shrink-0 bg-body-tertiary">
     <div class="d-flex align-items-center flex-shrink-0 p-3 link-body-emphasis text-decoration-none border-bottom">
-      <span class="fs-5 fw-semibold">List group</span>
+      <span class="fs-5 fw-semibold">Конфигурации СТМ</span>
     </div>
     <div class="list-group list-group-flush border-bottom scrollarea">
       <a href="#" class="list-group-item list-group-item-action active py-3 lh-sm" aria-current="true">
         <div class="d-flex w-100 align-items-center justify-content-between">
-          <strong class="mb-1">List group item heading</strong>
-          <small>Wed</small>
+          <strong class="mb-1">СТМ8-1П-А-1</strong>
+          <small></small>
         </div>
-        <div class="col-10 mb-1 small">Some placeholder content in a paragraph below the heading and date.</div>
       </a>
       <a href="#" class="list-group-item list-group-item-action py-3 lh-sm">
         <div class="d-flex w-100 align-items-center justify-content-between">
-          <strong class="mb-1">List group item heading</strong>
-          <small class="text-body-secondary">Tues</small>
+          <strong class="mb-1">СТМ8-1П-Б-2</strong>
+          <small class="text-body-secondary"></small>
         </div>
-        <div class="col-10 mb-1 small">Some placeholder content in a paragraph below the heading and date.</div>
       </a>
       <a href="#" class="list-group-item list-group-item-action py-3 lh-sm">
         <div class="d-flex w-100 align-items-center justify-content-between">
-          <strong class="mb-1">List group item heading</strong>
-          <small class="text-body-secondary">Mon</small>
+          <strong class="mb-1">СТМ8-2Л-А-1</strong>
+          <small class="text-body-secondary"></small>
         </div>
-        <div class="col-10 mb-1 small">Some placeholder content in a paragraph below the heading and date.</div>
       </a>
     </div>
   </div>
